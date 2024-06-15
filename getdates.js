338b165e9efd5b3921d1e44604ed2738520cd12c
ochengevaristus <145368846+ochengevaristus@users.noreply.github.com>
2024-06-15 12:44:00 +0300

    Add files via upload

---
+++ getdates.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+    document.getElementById('lastModified').textContent = "Last Modified: " + document.lastModified;
+});
